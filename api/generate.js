39797fab8be1cdf69970069f93841e22e18524e4
Hyperlink402 <Hyperlink484@gmail.com>
2026-07-27 14:43:04 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,4 @@
 export default async function handler(req, res) {
-  res.setHeader('Cache-Control', 's-maxage=300, stale-while-revalidate=60');
   if (req.method !== 'GET' && req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
